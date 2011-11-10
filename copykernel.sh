afa3660c44559bc5e7dcf314b5cf1f50809525a0
ardatdat <ardatdat@gmail.com>
2011-11-11 00:15:31 +0800

    overclock: OC to 1.2GHz

--- copykernel.sh
+++
@@ -1,8 +0,0 @@
-
-cp ./arch/arm/boot/zImage  /home/yiudou/kitchen179/BOOT-EXTRACTED/
-cp ./drivers/bluetooth/bthid/bthid.ko  		/home/yiudou/kitchen179/BOOT-EXTRACTED/boot.img-ramdisk/lib/modules/
-cp ./drivers/net/wireless/bcm4330/dhd.ko  	/home/yiudou/kitchen179/BOOT-EXTRACTED/boot.img-ramdisk/lib/modules/
-cp ./drivers/scsi/scsi_wait_scan.ko  		/home/yiudou/kitchen179/BOOT-EXTRACTED/boot.img-ramdisk/lib/modules/
-cp ./drivers/misc/fm_si4709/Si4709_driver.ko  	/home/yiudou/kitchen179/BOOT-EXTRACTED/boot.img-ramdisk/lib/modules/
-
-
