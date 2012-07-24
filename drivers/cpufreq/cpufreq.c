eb34b845d1d67e674ddb5868dc65b912f0228f6a
ardatdat <ardatdat@gmail.com>
2012-07-24 15:22:39 +0800

    OC: Add in-between frequencies: 1100 & 1200Mhz

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -50,11 +50,11 @@
 #define __CPUFREQ_KOBJ_DEL_DEADLOCK_FIX
 
 /* Initial implementation of userspace voltage control */
-#define FREQCOUNT 9
+#define FREQCOUNT 11
 #define CPUMVMAX 1250
 #define CPUMVMIN 770
-int cpufrequency[FREQCOUNT] = { 1320000, 1000000, 912000, 816000, 760000, 608000, 456000, 312000, 216000 };
-  int cpuvoltage[FREQCOUNT] = { 1250, 1125, 1050, 1000, 975, 900, 825, 775, 770 };  //UV, but 4th increased by 25mV 
+int cpufrequency[FREQCOUNT] = { 1320000, 1200000, 1100000, 1000000, 912000, 816000, 760000, 608000, 456000, 312000, 216000 };
+  int cpuvoltage[FREQCOUNT] = { 1250, 1200, 1150, 1125, 1050, 1000, 975, 900, 825, 775, 770 };  //UV, but 4th increased by 25mV 
 
 int cpuuvoffset[FREQCOUNT] = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
@@ -658,7 +658,7 @@ static ssize_t store_UV_mV_table(struct cpufreq_policy *policy, char *buf, size_
 {
 	int tmptable[FREQCOUNT];
 	int i;
-	unsigned int ret = sscanf(buf, "%d %d %d %d %d %d %d %d %d", &tmptable[0], &tmptable[1], &tmptable[2], &tmptable[3], &tmptable[4], &tmptable[5], &tmptable[6], &tmptable[7], &tmptable[8] );
+	unsigned int ret = sscanf(buf, "%d %d %d %d %d %d %d %d %d %d %d", &tmptable[0], &tmptable[1], &tmptable[2], &tmptable[3], &tmptable[4], &tmptable[5], &tmptable[6], &tmptable[7], &tmptable[8], &tmptable[9], &tmptable[10] );
 	if (ret != FREQCOUNT)
 		return -EINVAL;
 	for (i = 0; i < FREQCOUNT; i++)
